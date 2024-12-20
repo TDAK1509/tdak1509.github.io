a8640e5d1540238da52176de50b7c2a0172a72a0
Khuong Tran <dbcgdhh@gmail.com>
2024-12-20 15:54:29 +0700

    Use professional language

--- assets/AppPanel-TevxqsTj.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,o as t,c as a,t as o,j as l,i as p}from"./index-7nkX_KUw.js";const c={class:"app-panel"},r={key:0,class:"app-panel__title"},i={__name:"AppPanel",props:{title:String},setup(e){return(s,_)=>(t(),a("div",c,[e.title?(t(),a("h3",r,o(e.title),1)):l("",!0),p(s.$slots,"default",{},void 0)]))}},m=n(i,[["__scopeId","data-v-f198d3bb"]]);export{m as A};
