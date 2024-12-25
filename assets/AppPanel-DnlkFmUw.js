40d7677b4d832de0c22ed5d35ed07fe96f8630ee
Khuong Tran <dbcgdhh@gmail.com>
2024-12-25 09:43:03 +0700

    Update skills

---
+++ assets/AppPanel-DnlkFmUw.js
@@ -0,0 +1,1 @@
+import{_ as n,o as t,c as a,t as o,l,k as p}from"./index-W4I-bz_-.js";const c={class:"app-panel"},r={key:0,class:"app-panel__title"},_={__name:"AppPanel",props:{title:String},setup(e){return(s,i)=>(t(),a("div",c,[e.title?(t(),a("h3",r,o(e.title),1)):l("",!0),p(s.$slots,"default",{},void 0)]))}},m=n(_,[["__scopeId","data-v-f198d3bb"]]);export{m as A};
